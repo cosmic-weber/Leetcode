85e3f193e391f7f7a2856e8d46eb110f427fe06a
Yaman Birla <68591556+cosmic-weber@users.noreply.github.com>
2022-06-12 23:25:53 +0530

    Time: 44 ms (46.71%), Space: 14 MB (28.13%) - LeetHub

---
+++ 100-same-tree/100-same-tree.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        lst, lst1 = [], []
+        print(self.levelTraversal(p, lst))
+        print(self.levelTraversal(q, lst1))
+        
+        return lst == lst1
+        
+    def levelTraversal(self, m: Optional[TreeNode], lst) -> list:
+        
+        if m != None:
+            lst.append(m.val)
+            self.levelTraversal(m.left, lst)
+            self.levelTraversal(m.right, lst)
+        else:
+            lst.append(0)
+        return lst
