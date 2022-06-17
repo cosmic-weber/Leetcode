ed920f59080a36c7a4359cf1eccd9bf208685654
Yaman Birla <68591556+cosmic-weber@users.noreply.github.com>
2022-06-17 13:18:35 +0530

    Time: 48 ms (52.11%), Space: 13.8 MB (62.98%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -1,23 +1,37 @@
+from collections import Counter
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """        
         n = len(nums)
-        l, r = 0, n-1
         
-        i = 0
-        while i <= r:
-            if nums[i] == 0:
-                self.swap(l, i, nums)
-                l += 1
-            elif nums[i] == 2:
-                self.swap(r, i, nums)
-                r -= 1
-                i -= 1
-            i += 1
+        d = Counter(nums)
         
-    def swap(self, pointer, index, nums):
-        temp = nums[index]
-        nums[index] = nums[pointer]
-        nums[pointer] = temp
+        nums.clear()        
+        for i in range(d[0]):
+            nums.append(0)
+        for i in range(d[1]):
+            nums.append(1)
+        for i in range(d[2]):
+            nums.append(2)
+        
+#         l, r = 0, n-1
+        
+#         i = 0
+#         while i <= r:
+#             if nums[i] == 0:
+#                 self.swap(l, i, nums)
+#                 l += 1
+#             elif nums[i] == 2:
+#                 self.swap(r, i, nums)
+#                 r -= 1
+#                 i -= 1
+#             i += 1
+        
+#     def swap(self, pointer, index, nums):
+#         temp = nums[index]
+#         nums[index] = nums[pointer]
+#         nums[pointer] = temp
+        
+        
