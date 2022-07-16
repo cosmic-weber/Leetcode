02832b220c73afa724dc74651fb0e80ffa7216f2
Yaman Birla <68591556+cosmic-weber@users.noreply.github.com>
2022-07-16 18:32:59 +0530

    Time: 80 ms (6.56%), Space: 13.8 MB (60.10%) - LeetHub

--- 202-happy-number/202-happy-number.py
+++ 202-happy-number/202-happy-number.py
@@ -1,19 +1,19 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        return self.Algo(n, {})
+        return self.Algo(n, [])
     
-    def Algo(self, n, dict_):
+    def Algo(self, n, lst1):
         
         if n == 1:
             return True
-        if n in dict_:
+        if n in lst1:
             return False
             
         lst, s = [], str(n)
-        dict_[n] = 1
+        lst1.append(n)
         for c in s:
             lst.append(int(c))
         n = 0
         for i in lst:
             n += i ** 2
-        return self.Algo(n, dict_)
+        return self.Algo(n, lst1)
