20a13cee75c5db06b2e00d69a69cfab38295ec80
Yaman Birla <68591556+cosmic-weber@users.noreply.github.com>
2022-07-16 19:24:21 +0530

    Time: 64 ms (74.39%), Space: 16.1 MB (80.40%) - LeetHub

--- 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.py
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.py
@@ -8,15 +8,32 @@ class Node:
 
 class Solution:
     def preorder(self, root: 'Node') -> List[int]:
+        if not root:
+            return []
         lst = []
-        self.recursive(root, lst)
+        self.ll(root, lst)
         return lst
         
-    def recursive(self, root, lst):
-        if root is None:
-            return 
-        lst.append(root.val)
-        if root.children:
-            for child in root.children:
-                self.recursive(child, lst)
+    def ll(self, root, lst):
+        s = []
+        s.append(root)
+        while s:
+            node = s.pop()
+            lst.append(node.val)
+            if node.children:
+                s += node.children[::-1]
+
+# class Solution:
+#     def preorder(self, root: 'Node') -> List[int]:
+#         lst = []
+#         self.recursive(root, lst)
+#         return lst
+        
+#     def recursive(self, root, lst):
+#         if root is None:
+#             return 
+#         lst.append(root.val)
+#         if root.children:
+#             for child in root.children:
+#                 self.recursive(child, lst)
                 
