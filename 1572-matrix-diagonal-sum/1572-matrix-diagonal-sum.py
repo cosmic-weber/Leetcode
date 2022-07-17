fa397f14cb0c13534ebb1d2087d65c47a5635df7
Yaman Birla <68591556+cosmic-weber@users.noreply.github.com>
2022-07-17 14:04:03 +0530

    Time: 199 ms (28.15%), Space: 14.1 MB (93.31%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
@@ -1,31 +1,42 @@
 class Solution:
     def diagonalSum(self, mat: List[List[int]]) -> int:
-        n, sum_ = len(mat), 0
-        if n % 2 == 0:
-            i = 0
-            while i < n:
-                sum_ += mat[i][i]
-                i+=1
-            i -= 1
-            j = 0
-            while j < n and i >= 0:
-                sum_ += mat[i][j]
-                j += 1
-                i -= 1
-            return sum_
-        else:
-            i = 0
-            while i < n:
-                sum_ += mat[i][i]
-                i += 1
-            i -= 1
-            j = 0
-            while j < n and i >= 0:
-                if i == j:
-                    j += 1
-                    i -= 1
-                    continue
-                sum_ += mat[i][j]
-                j += 1
-                i -= 1
-            return sum_
+        n, ans = len(mat), 0
+        i, j = 0, n - 1
+        while i < n:
+            ans += mat[i][i]
+            ans += mat[i][j]
+            i += 1
+            j -= 1
+        if n % 2 != 0:
+            ans -= mat[(n-1)//2][(n-1)//2]
+        return ans
+    
+        # n, sum_ = len(mat), 0
+        # if n % 2 == 0:
+        #     i = 0
+        #     while i < n:
+        #         sum_ += mat[i][i]
+        #         i+=1
+        #     i -= 1
+        #     j = 0
+        #     while j < n and i >= 0:
+        #         sum_ += mat[i][j]
+        #         j += 1
+        #         i -= 1
+        #     return sum_
+        # else:
+        #     i = 0
+        #     while i < n:
+        #         sum_ += mat[i][i]
+        #         i += 1
+        #     i -= 1
+        #     j = 0
+        #     while j < n and i >= 0:
+        #         if i == j:
+        #             j += 1
+        #             i -= 1
+        #             continue
+        #         sum_ += mat[i][j]
+        #         j += 1
+        #         i -= 1
+        #     return sum_
